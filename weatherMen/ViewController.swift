98ab870923972df3daa4da55a2f00760934d8c9d
makenaw <makena.walsh@gmail.com>
2016-01-28 22:06:57 +0800

    alamofire request set up, but not successful due to being unable to set url

--- weatherMen/ViewController.swift
+++ weatherMen/ViewController.swift
@@ -8,22 +8,60 @@
 
 import UIKit
 import Alamofire
+import CoreLocation
 import MapKit
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     
 //    1. get user location using CLLocationManager
 //    2. use latitude and longitude to pull down data from weather api with alamofire
+//    2a. refactor weather into a 'weather' class?
 //    3. parse json
 //    4. reflect weather in UI
     
-    
+    var lat: String!
+    var lon: String!
 
+    var _dynamicEndPoint = ""
+
+    
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+            locationManager.startUpdatingLocation()
+        }
+    
     }
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let locValue:CLLocationCoordinate2D = manager.location!.coordinate
+        let lat = locValue.latitude
+        let lon = locValue.longitude
+        let dynamic = "\(BASE_URL)lat=\(lat)&lon=\(lon)\(APPID)"
+        _dynamicEndPoint = dynamic
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        print("\(_dynamicEndPoint)")
+        downloadWeather()
+    }
+    
+    func downloadWeather() {
 
+        let url = NSURL(string: _dynamicEndPoint)
+        Alamofire.request(.GET, url!).responseJSON { response in
+           let result = response.result
+            print(result.value.debugDescription)
+       
+            }
+    }
 }
 
---
+++ weatherMen/constants.swift
@@ -0,0 +1,14 @@
+//
+//  constants.swift
+//  weatherMen
+//
+//  Created by makena  on 1/28/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import Foundation
+
+let APPID = "&APPID=dd4f4a1bf5a6eb68d2e42e6bdf4c028b"
+let BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+
+typealias downloadComplete = () -> ()
